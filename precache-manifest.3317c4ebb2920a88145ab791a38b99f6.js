5654c6a494ec7bcfbc4ce4dc599e5fca9002fcd6
zeroslee21 <zeros.lee@gmail.com>
2020-09-30 21:34:02 +0900

    Updates

---
+++ precache-manifest.3317c4ebb2920a88145ab791a38b99f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d32f526fd007b7197f6162fc4ce374d",
+    "url": "/react_tpl_2020/index.html"
+  },
+  {
+    "revision": "629b48432e29441232e9",
+    "url": "/react_tpl_2020/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "a778923de067274d6fef",
+    "url": "/react_tpl_2020/static/js/2.35c55b87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_tpl_2020/static/js/2.35c55b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629b48432e29441232e9",
+    "url": "/react_tpl_2020/static/js/main.acccb064.chunk.js"
+  },
+  {
+    "revision": "58cc4be60268add7247d",
+    "url": "/react_tpl_2020/static/js/runtime-main.a0b1646c.js"
+  }
+]);
